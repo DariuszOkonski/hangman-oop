1e64ef2a8d31fe64d7da2eb7884d18a943a4f521
Dariusz Okonski <darek200180@gmail.com>
2020-09-12 18:55:01 +0100

    add guess method to Game class

--- app/Game.js
+++ app/Game.js
@@ -7,6 +7,17 @@ class Game {
     }
     
     start() {
-        console.log('Lets play a game!')
+        for (let i = 0; i < 26; i++) {
+            const label = (i + 10).toString(36);
+            const button = document.createElement('button');
+            button.innerHTML = label;
+            button.addEventListener('click', () => this.guess(label));
+            this.lettersWrapper.appendChild(button);  
+        }        
     }    
+
+    guess(letter) {
+        console.log(letter)
+        console.log(this);
+    }
 }
